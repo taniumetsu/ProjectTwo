86c39d57ecd9ad606906333f7005066abec94950
Tani Umetsu <taniu@hawaii.edu>
2023-10-24 14:12:15 -1000

    Completed 2nd Tab

--- Project Two/Project Two/secondViewController.swift
+++ Project Two/Project Two/secondViewController.swift
@@ -8,23 +8,37 @@
 import UIKit
 
 class secondViewController: UIViewController {
-
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var buttonOne: UIButton!
+    @IBOutlet weak var labelOne: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        labelOne.text = ""
+        textField.text = "Enter Degrees Fahrenheit"
+        
+        
         // Do any additional setup after loading the view.
     }
-    //* TESTTTTTT
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func buttonPressed(_ sender: Any) {
+        
+        var degreesC = Int(textField.text!)!
+        degreesC = degreesC * 9/5 + 32
+        
+        if let userInput = textField.text {
+           //* labelOne.text = userInput
+            labelOne.text = "Today Is \(degreesC) degrees Celcius"
+        }
+        
+      
+         // MARK: - Navigation
+         
+         // In a storyboard-based application, you will often want to do a little preparation before navigation
+         // Get the new view controller using segue.destination.
+         // Pass the selected object to the new view controller.
+         }
+        
+        
     }
-    */
 
-}
